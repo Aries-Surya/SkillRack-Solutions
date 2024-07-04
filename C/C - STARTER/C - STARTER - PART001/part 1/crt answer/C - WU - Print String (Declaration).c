d5ef70895980157d0fcb41bf42e3254c0c040ac6
Aries-Surya <ariessurya8124@gmail.com>
2024-07-04 20:22:57 +0530

    delete

--- C/C - STARTER/C - STARTER - PART001/part 1/crt answer/C - WU - Print String (Declaration).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char S[101];
-    printf(" ");
-
-scanf("%s",S)
-printf("%s",S)
-}
--- C/C - STARTER/C - STARTER - PART002/part 2/C - WU - Multiply by 5 (Error Identification) (Id-5616).c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num;
-    scanf("%d", &num);
-    printf("%d\n", num * 5); // Correct usage of printf to print an integer
-    return 0;
-}
